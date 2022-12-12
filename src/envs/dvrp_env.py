4bf32d9e6e9f18dcf74b74b2b842eb60e0040bea
Anna Konovalenko <annakonovlnko@gmail.com>
2022-12-12 00:19:02 +0000

    render but with bugs

--- src/envs/dvrp_env.py
+++ src/envs/dvrp_env.py
@@ -6,7 +6,10 @@ import os
 import numpy as np
 from ma_gym.envs.utils.action_space import MultiAgentActionSpace
 from ma_gym.envs.utils.observation_space import MultiAgentObservationSpace
-from .utils import dijkstra_paths
+from .utils import dijkstra_paths, fill_cell_im, draw_image
+import copy
+from .const import *
+from .draw import *
 
 # ACTION:
 # 0: Wait (Do nothing)
@@ -14,11 +17,18 @@ from .utils import dijkstra_paths
 # 2: Deliver order i (by moving one step towards the respective delivery location)
 
 class DVRPEnv(Env):
-    def __init__(self, n_agents = 2, episode_limit=144, expected_orders=15, grid_shape=(10, 10)):
+    def __init__(self, n_agents = 2, episode_limit=100, expected_orders=15, grid_shape=(10, 10)):
         self.n_agents = n_agents 
         self._episode_length = episode_limit
+        self._expected_orders = expected_orders
         self._grid_shape = grid_shape  # size of grid or map
         self.depot_location = (round(self._grid_shape[0] / 2), round(self._grid_shape[1] / 2))  # set at centre of map
+
+        #rewards
+        self.invalid_action_penalty = 5
+        self.delivery_reward = 2
+
+
 
         # General parameters (changes throughout episode)
         self._vehicle_episode_rewards = [0 for _ in range(self.n_agents)]
@@ -38,7 +48,6 @@ class DVRPEnv(Env):
         self._vehicle_mileage = [0] * self.n_agents
         self.vehicle_paths, self.vehicle_path_lengths = dijkstra_paths(self._grid_shape[0], self._grid_shape[1])
 
-
         # Limits for observation space variables
         self.vehicle_x_min = 0
         self.vehicle_y_min = 0
@@ -50,12 +59,23 @@ class DVRPEnv(Env):
         self.order_x_max = self._grid_shape[0] - 1
         self.order_y_max = self._grid_shape[0] - 1
         self.clock_max = self._episode_length
-       
 
+        # Generate homogenous generation of orders throughout episode
+        self.order_generation_window = self._episode_length   # time when orders can appear
+        self.order_prob = self._expected_orders / self.order_generation_window  # set order probability such that the expected number of orders per day = total_orders
+        self.generated_orders = 0
 
         # Order parameters
         self.orders_pos = [(-1, -1)] * self.n_orders
+        self.order_status = [-1] * self.n_orders
         self.order_time = [-1] * self.n_orders
+        self._total_appeared_orders = 0
+
+        #Render parameters
+        self.icon_av, _ = draw_image('av.png')
+        self.icon_pkg, _ = draw_image('pin.png')
+        # self.icon_pkg = self.icon_pkg.convert("RGBA")
+        self.viewer = None
 
         # Create observation space
         self._obs_high = np.array([self.vehicle_x_max, self.vehicle_y_max] +
@@ -78,11 +98,15 @@ class DVRPEnv(Env):
 
 
     def step(self, actions):
+        self.render()
         vehicles_action = list(actions)
         self.vehicles_action_history.append(vehicles_action)
         self._step_count += 1
         self._clock += 1
         self.rewards = [0] * self.n_agents
+
+        self.__update_orders()
+        self.__update_vehicles_pos(vehicles_action)
 
         if self._clock >= self._episode_length:
             for i in range(self.n_agents):
@@ -91,6 +115,17 @@ class DVRPEnv(Env):
         self._vehicle_episode_rewards = [a + b for a, b in zip(self._vehicle_episode_rewards, self.rewards)]
 
         return self.get_state(), self.rewards, self._vehicle_dones, {'ongoing': True}
+
+    def __update_orders(self):
+        if (self._clock < self.order_generation_window) and (self.generated_orders < self._expected_orders):
+            order_x, order_y = self.__generate_order()
+            order_i = self.order_status.index(-1)
+            # for order_i in range(self.n_orders):
+            #     if self.order_status[order_i] == -1:
+            self.generated_orders += 1
+            self.order_time[order_i] = self._clock
+            self.orders_pos[order_i] = (order_x, order_y)
+            self.order_status[order_i] = 0
 
     def get_stats(self):
         return {}
@@ -112,7 +147,7 @@ class DVRPEnv(Env):
             pos = self.vehicles_pos[vehicle_i]
             flat_list = [pos[0], pos[1]]
             flat_list.extend([x[0] for x in self.orders_pos])
-            flat_list.extend([y[0] for y in self.orders_pos])
+            flat_list.extend([y[1] for y in self.orders_pos])
             flat_list.extend([self._clock])
             _obs.append(flat_list)
         return _obs
@@ -121,10 +156,23 @@ class DVRPEnv(Env):
         return self.get_obs_size()*self.n_agents
 
     def get_avail_actions(self):
-        return [[1] * self.get_total_actions()] * self.n_agents
+        avail_actions = [0] * self.get_total_actions()
+        avail_actions[0] = 1
+        avail_actions[1] = 1
+        for index, order_i in enumerate(self.order_status):
+            if order_i == 0:
+                avail_actions[index + 2] = 1
+        return avail_actions * self.n_agents
+        # return [[1] * self.get_total_actions()] * self.n_agents
 
     def get_avail_agent_actions(self, agent_id):
-        return [1] * self.get_total_actions()
+        avail_actions = [0] * self.get_total_actions()
+        avail_actions[0] = 1
+        avail_actions[1] = 1
+        for index, order_i in enumerate(self.order_status):
+            if order_i == 0:
+                avail_actions[index+2] = 1
+        return avail_actions
 
     def get_total_actions(self):
         return self.action_max
@@ -140,10 +188,10 @@ class DVRPEnv(Env):
 
         # Order parameters
         self.orders_pos = [(-1, -1)] * self.n_orders
+        self.order_status = [-1] * self.n_orders
         self.order_time = [-1] * self.n_orders
-
-
-        self.__init_vehicles()
+        self._total_appeared_orders = 0
+        self.generated_orders = 0
 
         # Vehicle parameters
         self.vehicles_action_history = []
@@ -152,6 +200,10 @@ class DVRPEnv(Env):
         self._successful_delivery = 0
         self._vehicle_mileage = [0] * self.n_agents
         self.vehicle_paths, self.vehicle_path_lengths = dijkstra_paths(self._grid_shape[0], self._grid_shape[1])
+
+        self.__draw_base_img()
+        self.images = [self._base_img]
+        self.__init_vehicles()
 
         return self.get_state()
 
@@ -179,13 +231,14 @@ class DVRPEnv(Env):
             self.__vehicle_to_depot(vehicle_i)
         elif 2 <= vehicle_action <= self.action_max:  # deliver order i: move one step towards order i
             order_i = vehicle_action - 2
-            self.__vehicle_to_order(vehicle_i, order_i)
+            if self.order_status[order_i] == 0:
+                self.__vehicle_to_order(vehicle_i, order_i)
 
     def __vehicle_to_order(self, vehicle_i, order_i):
         current_pos = self.vehicles_pos[vehicle_i]
         order_i_pos = self.orders_pos[order_i]
         if current_pos == order_i_pos:
-            pass  # nth happens if already at order position
+            self.order_status[order_i] = 1 #delivered
         else:
             self._vehicle_mileage[vehicle_i] += 1
             next_pos = (self.vehicle_paths[current_pos][order_i_pos][1][0],
@@ -198,17 +251,39 @@ class DVRPEnv(Env):
             pass
         else:
             self._vehicle_mileage[vehicle_i] += 1
-            self._total_mileage += 1
             next_pos = (self.vehicle_paths[current_pos][self.depot_location][1][0],
                         self.vehicle_paths[current_pos][self.depot_location][1][1])
             self.vehicles_pos[vehicle_i] = next_pos
 
 
     def render(self):
-        pass
+        img = copy.copy(self._base_img)
+
+        # Agents
+        for agent_i in range(self.n_agents):
+            fill_cell_im(img, self.icon_av, self.vehicles_pos[agent_i], cell_size=CELL_SIZE)
+
+        # Orders
+        for idx, j in enumerate(self.order_status):
+            if j != -1:
+                fill_cell_im(img, self.icon_pkg, self.orders_pos[idx], cell_size=CELL_SIZE)
+
+        self.images.append(img)
+        # img = np.asarray(img)
+        img.show()
+        img.save('gridworld.jpg', format='JPEG', subsampling=0, quality=100)
+
+        # from gym.envs.classic_control import rendering
+        # if self.viewer is None:
+        #     self.viewer = rendering.SimpleImageViewer()
+        #     self.viewer.imshow(img)
+        # return self.viewer.isopen
 
     def close(self):
         pass
+
+    def __draw_base_img(self):
+        self._base_img = draw_grid(self._grid_shape[0], self._grid_shape[1], cell_size=CELL_SIZE, fill='white')
 
     def seed(self, seed):
         self.seed = seed
--- src/envs/utils.py
+++ src/envs/utils.py
@@ -2,7 +2,15 @@ import networkx as nx
 import numpy as np
 import pickle
 import pandas as pd
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw
 
+path = 'envs/rebalance/'
+# xmin, xmax = -74.03, -73.89
+# ymin, ymax = 40.69, 40.89
+
+xmin, xmax = 100, 5
+ymin, ymax = 100, 5
 def dijkstra_paths(n, m):
 
     G = nx.grid_2d_graph(n, m)
@@ -10,3 +18,75 @@ def dijkstra_paths(n, m):
     paths = dict(nx.all_pairs_dijkstra_path(G))
     lengths = dict(nx.all_pairs_dijkstra_path_length(G))
     return paths, lengths
+
+def load_graph(file_name):
+    with open(path+file_name, 'rb') as pickle_file:
+        G = pickle.load(pickle_file)
+        nodes = list(G.nodes())
+        edges = list(G.edges())
+        A = nx.adjacency_matrix(G, nodelist=sorted(G.nodes()))
+    return G, A
+
+
+def draw_map(file_name):
+    with open(file_name, "rb") as f:
+        load = pickle.load(f)
+    fig, ax = plt.subplots(figsize=(50, 50))
+    load[1].plot(ax=ax,alpha=0.2)
+    fig.show()
+    fig.canvas.draw()
+    return fig, ax
+
+
+def draw_image(file_name):
+    im = Image.open(path+file_name)
+    width, height = im.size
+    scale_x = width / (xmax - xmin)
+    scale_y = height / (ymax - ymin)
+    scale = [scale_x, scale_y]
+    return im, scale
+
+
+def draw_vehicle(image, pos, scale, fill = '#ff6361', r=6):
+    scale_x = scale[0]
+    scale_y = scale[1]
+    y = image.size[1] - (pos[0]-ymin)*scale_y
+    x = (pos[1]-xmin)*scale_x
+    x_dash = x + r
+    y_dash = y + r
+    ImageDraw.Draw(image).ellipse([(x-r, y-r), (x_dash, y_dash)], outline=fill, fill=fill)
+
+
+def draw_order(image, pos, scale, fill='#003f5c', r = 4):
+    scale_x = scale[0]
+    scale_y = scale[1]
+    y = image.size[1] - (pos[0]-ymin)*scale_y
+    x = (pos[1]-xmin)*scale_x
+    x_dash = x + r
+    y_dash = y + r
+    ImageDraw.Draw(image).rectangle([(x-r, y-r), (x_dash, y_dash)], outline=fill, fill=fill)
+
+
+def draw_map_h3(df):
+    fig, ax = plt.subplots(figsize=(8, 8))
+    df[['h3_res9', 'geometry']].plot(alpha=0.3, ax=ax, facecolor="none",edgecolor='#444e86')
+
+    # fig.show()
+    # fig.canvas.draw()
+    return fig, ax
+
+
+def load_file(file_name):
+    with open(path+file_name, 'rb') as pickle_file:
+        h3 = pickle.load(pickle_file)
+    return h3
+
+def fill_cell_im(image, icon, pos, cell_size=None, fill='black', margin=0):
+    # assert cell_size is not None and 0 <= margin <= 1
+
+    col, row = pos
+    row, col = row * cell_size, col * cell_size
+    # margin *= cell_size
+    # x, y, x_dash, y_dash = row + margin, col + margin, row + cell_size - margin, col + cell_size - margin
+    # ImageDraw.Draw(image).rectangle([(x, y), (x_dash, y_dash)], fill=fill)
+    image.paste(icon, (int(row)+1, int(col)+1), icon)
